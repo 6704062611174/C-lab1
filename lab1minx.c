83c27404e8593172251aa4c76519e4acc03015e1
6704062611174 <s6704062611174@email.kmutnb.ac.th>
2024-11-28 14:37:25 +0700

    commit4
    ส่งข้อ 3

--- lab1minx.c
+++ lab1minx.c
@@ -3,10 +3,8 @@
 int main() {
 	int n1;
 	int n2;
-	printf("input num1 : ");
-	scanf("%d", &n1);
-	printf("input num2 : ");
-	scanf("%d", &n2);
+	printf("input num1 and num2: ");
+	scanf("%d %d", &n1,&n2);
 	if (n1 > n2){
 		printf("n1 is n2 more than %d",n1 - n2);
 	}
