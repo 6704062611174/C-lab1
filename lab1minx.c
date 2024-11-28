7ce5391096ecffad2b43fedb97a4e0ddcb4e4fda
6704062611174 <s6704062611174@email.kmutnb.ac.th>
2024-11-28 14:20:36 +0700

    commit 1 great

---
+++ lab1minx.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int num1 = 5;
+	int num2 = 14;
+	if (num1 > num2){
+		printf("num1 greater than num2");
+	}
+	if (num1 < num2){
+		printf("num2 greater than num1");
+	}
+}
+
+
